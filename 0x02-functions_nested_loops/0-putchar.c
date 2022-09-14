af8ac07679564f8784bc8ad1b245d89a7a4a8848
root <Kufresundayeyen@gmail.com>
2022-09-14 11:36:03 -0700

    function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
- *                                                                                                                                      
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('c');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
