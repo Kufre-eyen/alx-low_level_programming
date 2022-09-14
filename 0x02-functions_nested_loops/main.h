d575c1497cffba534492aa54e37692d4796750f0
root <Kufresundayeyen@gmail.com>
2022-09-14 06:59:08 -0700

    function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,133 @@
+nt _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+vim O-putchar.c
+
+#include <stdio.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *  code description
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+		putchar('_');
+
+			putchar('p');
+
+				putchar('u');
+
+					putchar('t');
+
+						putchar('c');
+
+							putchar('h');
+
+								putchar('a');
+
+									putchar('r');
+
+										putchar('\n');
+
+											return (0);
+
+}
+
+nt _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+vim O-putchar.c
+
+#include <stdio.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *  code description
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+		putchar('_');
+
+			putchar('p');
+
+				putchar('u');
+
+					putchar('t');
+
+						putchar('c');
+
+							putchar('h');
+
+								putchar('a');
+
+									putchar('r');
+
+										putchar('\n');
+
+											return (0);
+
+}
+
+
